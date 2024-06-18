ebccdd753eddeb35538eb84738439a64ef59da87
suhail19-oss <118662475+suhail19-oss@users.noreply.github.com>
2024-06-18 10:02:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Book-Recommender-System
+# Book_Recommender
+Book recommendation system to recommend top books based on rating format
---
+++ app.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError");
+const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
+const People = require("./models/people.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/bookrecommendor";
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+//Login & SignUp page
+app.get("/", (req, res) => {
+    res.render("listings/login.ejs");
+});
+
+//Index Route
+app.get("/listings", async(req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
+
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
+//Reviews 
+//[POST Review Route]
+app.post("/listings/:id/reviews", async (req, res) => {       // router.post("...", validateReview, wrapAsync(...));
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+});
+
+//Delete Review Route
+app.delete("/listings/:id/review/:reviewId", async (req, res) => {
+    let {id, reviewId} = req.params;
+
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+});
+
+//Recommendation
+app.get("/recommend", (req, res) => {
+    res.render("listings/recommends.ejs");
+})
+
+//Sign-Up
+app.get("/signUp", (req,res) => {
+    res.render("listings/signup.ejs");
+});
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+});
+
+app.use((err, req, res, next) => {
+    let {statusCode=500, message="Something went wrong"} = err;
+    res.status(statusCode).render("error.ejs", {message});
+    //res.status(statusCode).send(message);
+});
+
+app.listen(3000, () => {
+    console.log("server is listening to port 3000");
+});
